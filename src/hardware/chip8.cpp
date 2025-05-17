743e4543c78cb467d0a21e7c2b1069509f131d96
Alb33rt <albert931023@gmail.com>
2025-05-16 20:20:00 -0400

    Configured repo and CMake

---
+++ src/hardware/chip8.cpp
@@ -0,0 +1,14 @@
+//
+// Created by albert on 5/16/25.
+//
+
+#include "chip8.h"
+
+namespace chip {
+    class Chip8 {
+    public:
+
+    private:
+
+    };
+} // chip
---
+++ src/hardware/chip8.h
@@ -0,0 +1,12 @@
+//
+// Created by Alb33rt on 5/16/25.
+//
+
+#ifndef CHIP8_H
+#define CHIP8_H
+
+namespace chip {
+
+} // chip
+
+#endif //CHIP8_H
